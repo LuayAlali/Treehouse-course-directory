eb7dca1e3fe02a727e5503d3c48e7a1a7b487bb5
Luay Alali <luayalali@gmail.com>
2020-11-10 14:44:13 -0800

    Links within Course.js now showcase corresponding components. Used NavLink to apply active style once clicked.

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import {
+  NavLink,
+  Route
+
+} from 'react-router-dom';
+//  components imports
+import JavaScript from './courses/JavaScript';
+import CSS from './courses/CSS';
+import HTML from './courses/HTML';
 
 const Courses = () => (
+
   <div className="main-content courses">
     <div className="course-header group">
       <h2>Courses</h2> 
       <ul className="course-nav">
-        <li><a href='/courses/html'>HTML</a></li>
-        <li><a href='/courses/css'>CSS</a></li>
-        <li><a href='/courses/javascript'>JavaScript</a></li>
+        <li><NavLink to='/courses/html'>HTML</NavLink></li>
+        <li><NavLink to='/courses/css'>CSS</NavLink></li>
+        <li><NavLink  to='/courses/javascript'>JavaScript</NavLink></li>
       </ul>
     </div>
     
     {/* Write routes here... */}
+ <Route path="/courses/html" component={HTML} />
+ <Route path="/courses/css" component={CSS} />
+ <Route path="/courses/javascript" component={JavaScript} />
+
   </div>
+  
 );
 
 export default Courses;
