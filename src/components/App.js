44c49a7b6f28738f6c42dd9e255e03b2d189bbc2
Luay Alali <luayalali@gmail.com>
2020-11-10 14:10:10 -0800

    Route path for nav links.  Link to components in header using 'Link to"

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,22 @@ import {
 
 } from 'react-router-dom';
 
+// App component
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Teachers from './Teachers';
+import Courses from './Courses';
+
 
 const App = () => (
   <BrowserRouter>
   <div className="container">
-
+    <Header />
+     <Route exact path="/" component={Home}/>
+     <Route path="/about" render={ () => <About title="About" /> } />
+     <Route  path="/teachers" component={Teachers} />
+     <Route  path="/courses" component={Courses} />
   </div>
   </BrowserRouter>
 );
